8a80fd5f9804b6c80157c686fc6590091e58108e
Andrew Scott Cohen <andrewscohen@uchicago.edu>
2022-04-29 16:11:34 +0000

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
 
+  get("/wizard_add", { :controller => "application", :action => "addition_results" })
+  
+  get("/add", { :controller => "application", :action => "addition_form" })
+
+  get("/wizard_street_to_coords", { :controller => "application", :action => "street_to_coords_results" })
+  
+  get("/street_to_coords/new", { :controller => "application", :action => "street_to_coords_form" })
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
